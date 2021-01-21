195e152b803e011023e5bd7123c98c4d9dc6280e
Raehan <raehan900809@gmail.com>
2021-01-21 16:37:31 +0900

    Add comment

--- StringCheck.js
+++ StringCheck.js
@@ -1,5 +1,10 @@
 let solution = (s) => {
+  console.log(s.split("").map(char => isNaN(char)));
   return s.length === 4 || s.length === 6 ? !s.split("").find(char => isNaN(char)) : false;
+  // 길이가 4, 6인지 우선 확인
+  // split 함수를 통해서 문자열을 하나씩 쪼개서 배열로 만든다.
+  // 그 후 배열 요소를 isNaN을 통해 숫자가 아닌게 있는지 확인을 하고 있다면 false를 반환한다.
+  // 문자열 자체로 확인하지 않고 쪼개서 한 이유는 자바스크립트는 숫자형이 따로 없고 부동소수점을 사용하기 때문에 e가 들어간 지수도 숫자로 인식하기 때문이다.
 }
 
 let testCaseList = [
@@ -13,5 +18,7 @@ let testCaseList = [
 
 for (const testCase of testCaseList) {
   let result = solution(testCase.case);
+  console.log(`result ${result}`)
   console.log(result === testCase.result)
 }
+
